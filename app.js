696285f0272987c5856b81b61096381357869539
Jered McCullough <mathias.armstrong@gmail.com>
2014-07-01 16:16:56 -0700

    replaced routes with home file to test out params

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// var routes = require('./routes/index');
+var home = require('./routes/home');
+var user = require('./routes/user');
 
 var app = express();
 
@@ -21,8 +22,8 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', home);
+app.use('/user', user);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
