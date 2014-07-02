692d4f0760af28a113b6125866e487bcec7a923b
Jered McCullough <mathias.armstrong@gmail.com>
2014-07-01 21:10:40 -0700

    help

--- routes/home.js
+++ routes/home.js
@@ -6,6 +6,9 @@ router.get('/', function(req, res) {
   res.render('home', { title: 'Politics 101' });
 });
 
+router.post('')
+
+
 router.post('/test', function(req, res) {
   console.log(req.body);
   res.render('user', req.body);
